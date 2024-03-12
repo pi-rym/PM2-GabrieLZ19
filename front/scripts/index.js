882dacbe8412961fc13cbe07ae79d22500ab6324
Gabriel Lazo <gabriellazo48@gmail.com>
2024-03-12 01:07:00 -0300

    integracion con axios agregada

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,19 @@
 const crearCards = require("./crearCards");
+const axios = require("axios");
 
-$.get("https://students-api.up.railway.app/movies", (datos) =>
-  datos.forEach(crearCards)
-);
+// $.get("https://students-api.up.railway.app/movies", (datos) =>
+//   datos.forEach(crearCards)
+// );
+
+const fetchData = async () => {
+  try {
+    const datos = await axios.get("https://students-api.up.railway.app/movies");
+    // console.log(datos.data);
+    const pelis = datos.data;
+    pelis.forEach(crearCards);
+  } catch (error) {
+    error.message;
+  }
+};
+
+fetchData();
