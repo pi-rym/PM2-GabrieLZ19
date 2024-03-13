5b06122fad00d56a1015a46e4a72a498edd98ca1
Gabriel Lazo <gabriellazo48@gmail.com>
2024-03-13 10:10:58 -0300

    request y response agregados

--- back/src/controllers/controllers-index.js
+++ back/src/controllers/controllers-index.js
@@ -1,5 +1,7 @@
-function controladorMovies() {
-  console.log("próximamente estarán disponibles los datos de películas.");
+function controladorMovies(req, res) {
+  res
+    .status(200)
+    .send("próximamente estarán disponibles los datos de películas."); //status 200 es todo OK
 }
 
 module.exports = { controladorMovies };
