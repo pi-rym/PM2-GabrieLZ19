e1594cd4814b2cc994bc7232248ebd8070df41c2
Gabriel Lazo <gabriellazo48@gmail.com>
2024-03-14 12:27:12 -0300

    funcion insertada en module.exports

--- back/src/controllers/moviesControllers.js
+++ back/src/controllers/moviesControllers.js
@@ -2,7 +2,7 @@ const moviesServices = require("../services/moviesServices");
 
 async function controladorMovies(req, res) {
   try {
-    const movies = await moviesServices();
+    const movies = await moviesServices.obtenerMovies();
     res.status(200).json(movies); //status 200 es todo OK
   } catch (error) {
     res.status(500).json({ error: error.message });
--- back/src/services/moviesServices.js
+++ back/src/services/moviesServices.js
@@ -1,12 +1,28 @@
 const axios = require("axios");
 
-const obtenerMovies = async () => {
-  try {
-    const movies = await axios("https://students-api.up.railway.app/movies");
-    return movies.data;
-  } catch (error) {
-    console.log(error.message);
+class Movies {
+  constructor(title, poster, director) {
+    this.title = title;
+    this.poster = poster;
+    this.director = director;
+    this.movies = [];
   }
-};
 
-module.exports = obtenerMovies;
+  async guardarMovies() {
+    const movies = await axios("https://students-api.up.railway.app/movies");
+    return this.movies.push(movies);
+  }
+}
+
+module.exports = {
+  obtenerMovies: async () => {
+    try {
+      const { data } = await axios(
+        "https://students-api.up.railway.app/movies"
+      );
+      return data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+};
